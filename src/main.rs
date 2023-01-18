3f40227f85cfcc1a746eb787a46364f4ac9aedb3
HistidineDwarf <dvorakdwarf@gmail.com>
2023-01-17 18:19:13 -0800

    Quick error message change

--- src/main.rs
+++ src/main.rs
@@ -7,11 +7,11 @@ use ui::gui;
 fn main() {
     println!("Please paste in your token. If you don't know what that is, please google");
     let mut token = String::new();
-    std::io::stdin().read_line(&mut token).expect("Wrong token");
+    std::io::stdin().read_line(&mut token).expect("Could not read input");
     token.pop(); //get rid of \n on the end
 
     //Simplified way of passing token and client
     let conn = Connection::new(&token); 
 
-    gui::summon_gooey(conn).expect("Could not run");
+    gui::summon_gooey(conn).expect("Could not run the main script. Possibly incorrect token.");
 }
